c4622993c8368ee18a1f5b2ed24a59ff71a83e87
Muzahidul Islam <mujahidul.core@gmail.com>
2022-05-08 22:23:52 +0600

    genius car server repo

---
+++ __server_deploy.js
@@ -0,0 +1,13 @@
+/**
+ * --------------------
+ * ONE TIME for your computer
+ * --------------------
+ * 1. create heroku account
+ * 2. verify email
+ * 3. install heroku CLI
+ * 4. heroku login
+ * --------------------
+ * for each project one time
+ * --------------------
+ * 1. heroku create
+*/
